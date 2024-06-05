127d24ea30384acbdbb3bfaabc8d691d5d5a9c66
Quinn Las <quinn.s.las@gmail.com>
2024-06-04 21:03:28 -0500

    setup github actions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+
+    // https://vitejs.dev/guide/static-deploy.html#github-pages
+    // this should be the repo name
+    base: '/city-learner/'
 })
