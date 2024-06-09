3fdb7b7a4f228d3e18ec97594d22e5c203d6ba74
Quinn Las <quinn.s.las@gmail.com>
2024-06-08 20:01:40 -0500

    add loading message

--- src/CityChooser.jsx
+++ src/CityChooser.jsx
@@ -11,6 +11,7 @@ export default function CityChooser() {
     const [streets, setStreets] = useState(null)
     const [justStarted, setJustStarted] = useState(true)
     const [showCanvas, setShowCanvas] = useState(false)
+    const [showLoading, setShowLoading] = useState(false)
 
     function onInput(e) {
         setSearchText(e.target.value)
@@ -21,7 +22,9 @@ export default function CityChooser() {
     }
 
     async function onClickSearch() {
+        setShowLoading(true)
         setSearchResults(await searchCities(searchText))
+        setShowLoading(false)
         setShowSearchResults(true)
     }
 
@@ -30,11 +33,13 @@ export default function CityChooser() {
         const result = searchResults[i]
 
         // get the city data from osm
+        setShowLoading(true)
         const borderRes = await getCityBorders(result.osm_id)
         setBorders(borderRes.data.elements[0])
 
         await new Promise((rs) => setTimeout(rs, 1000))
         const streets = await getRoads(result.osm_id)
+        setShowLoading(false)
         setStreets(streets)
         setShowCanvas(true)
         setSearchResults([])
@@ -43,9 +48,9 @@ export default function CityChooser() {
 
     return (
         <>
-            <div style={{ textAlign: "-webkit-center"}}>
+            <div style={{ textAlign: "-webkit-center" }}>
                 {justStarted ? (
-                    <div style={{marginTop:"100px"}}>
+                    <div style={{ marginTop: "100px" }}>
                         <h1>City Learner</h1>
                         <p>Enter a city to get started.</p>
                     </div>
@@ -70,10 +75,10 @@ export default function CityChooser() {
                         }}
                     />
                     {showSearchResults && !searchResults.length ? (
-                        <p style={{textAlign: 'left'}}>
-                            No city search results were found. Please try a different
-                            search. Some cities do not have borders in OpenStreetMap
-                            yet.
+                        <p style={{ textAlign: "left" }}>
+                            No city search results were found. Please try a
+                            different search. Some cities do not have borders in
+                            OpenStreetMap yet.
                         </p>
                     ) : (
                         <></>
@@ -104,6 +109,13 @@ export default function CityChooser() {
                     width={1440 - 40 * 2 - 32 * 2}
                     height={843 - 32 * 2 - 10}
                 />
+            ) : (
+                <></>
+            )}
+            {showLoading ? (
+                <div style={{ position: "fixed", bottom: "8px" }}>
+                    Loading...
+                </div>
             ) : (
                 <></>
             )}
